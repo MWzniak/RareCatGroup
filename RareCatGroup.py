5f5c7195c62c8b528155eaee774ddfebaf466515
MWzniak <98286164+MWzniak@users.noreply.github.com>
2022-11-15 10:37:39 +0100

    Add files via upload

---
+++ RareCatGroup.py
@@ -0,0 +1,71 @@
+class RareCatGroup():
+    '''
+    A class for grouping categories with certain amount of occurences in a column.
+
+    This class might come in handy dealing with numerous categories. 
+    When you are faced with a lot of rare categories, OneHotEncoding might create too many dimensions. 
+    Mapping these as 'other' drastically reduces dimensionality of the dataset.
+    '''
+    def __init__(self,dataset,amount_of_obs,col_list=False,target_variable=False,map_value='other'):
+        '''
+        Constructs all the necessary attributes.
+
+        Parameters
+        ----------
+        dataset : pandas.DataFrame object
+            Dataframe which you want to modify.
+        
+        amount_of_obs : int
+            Categories with less than this amount will be converted to 'other'
+
+        col_list : list, optional
+            Colums of the dataset which you want to be altered. 
+            Defeault = False, every column in the dataset will be altered.
+        
+        target_variable: string, optional
+            Specify the column name of the target variable. This column will be left unchanged.
+            Defeault = False, all entered columns will be altered.
+
+        map_value: string, optional
+            Specify the replacement value of the rare categories
+            Defeault = 'other'
+        '''
+        self.name_list=[]   
+        self.dict_list=[]
+        self.am=amount_of_obs
+        self.dataset=dataset
+        if col_list:
+            self.cats=col_list
+        else:
+            self.cats=list(self.dataset.columns)
+        if target_variable:
+            self.cats.remove(target_variable)
+        self.map_value=map_value
+    def make_dict(self):
+        '''
+        Create a dictionary of values and map_values for each column
+        '''
+        for n in self.cats:
+            val_count=self.dataset[n].value_counts()
+            d={}
+            if len(val_count[val_count<self.am].index)>1:
+                self.name_list.append(val_count[val_count<self.am].name)
+                s=list(pd.Series(val_count[val_count<self.am].index))
+                for m in range(0,len(s)):
+                    d[s[m]]=self.map_value
+                self.dict_list.append(d)
+        return [self.dict_list,self.name_list]
+    def group_cats(self):
+        '''
+        Group rare categories.
+
+        Returns
+        -------
+        dataset: pandas.DataFrame object
+            The modified dataframe.
+        '''
+        l=self.make_dict()
+        dict_list=l[0]
+        for n in range(0,len(dict_list)):
+            self.dataset[self.name_list[n]]=self.dataset[self.name_list[n]].map(dict_list[n]).fillna(self.dataset[self.name_list[n]])
+        return self.dataset
